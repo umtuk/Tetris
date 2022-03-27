0f91b829d3452d8475737869de657bfe66169be7
komh0086 <50826800+komh0086@users.noreply.github.com>
2022-03-27 16:12:47 +0900

    Score getter/setter class

--- app/src/main/java/Tetris/domain/score/entity/Score.java
+++ app/src/main/java/Tetris/domain/score/entity/Score.java
@@ -1,7 +1,33 @@
 package Tetris.domain.score.entity;
 
 public class Score {
+    private String UserName;
+    private double GameEndTime;
+    private int Score;
     
-    private String username;
-    private int score;
+    public Score(String new_UserName, double new_GameEndTime, int new_Score){
+        this.UserName = new_UserName;
+        this.GameEndTime = new_GameEndTime;
+        this.Score = new_Score;
+    }
+
+    public String Get_UserName(){
+        return this.UserName;
+    }
+    public double Get_GameEndTime(){
+        return this.GameEndTime;
+    }
+    public int Get_Score(){
+        return this.Score;
+    }
+
+    public void Set_UserName(String new_UserName){
+        this.UserName = new_UserName;
+    }
+    public void Set_GameEndTime(double new_GameEndTime){
+        this.GameEndTime = new_GameEndTime;
+    }
+    public void Set_Score(int new_Score){
+        this.Score = new_Score;
+    }
 }
