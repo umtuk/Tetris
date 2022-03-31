40a492926330526bbc807111ebe2b40e4aacd6e5
komh0086 <50826800+komh0086@users.noreply.github.com>
2022-03-31 16:13:59 +0900

    keyevent

--- app/src/main/java/Tetris/view/actionListener/KeyListener.java
+++ app/src/main/java/Tetris/view/actionListener/KeyListener.java
@@ -1,5 +1,19 @@
 package Tetris.view.actionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class KeyListener {
-    
+    public void mainFrameKeyEvent(KeyEvent e){
+        switch(e.getKeyCode()){
+            case KeyEvent.VK_UP:
+
+                break;
+            case KeyEvent.VK_DOWN:
+            
+                break;
+            default:
+                break;
+        }
+    }
 }
