1eafa9e83e53df4af41be3f589f27edd01283651
Taeuk Um <42173347+umtuk@users.noreply.github.com>
2022-03-31 15:13:36 +0900

    Create some service for JFrame Dev

--- app/src/main/java/Tetris/global/config/service/ConfigService.java
+++ app/src/main/java/Tetris/global/config/service/ConfigService.java
@@ -3,29 +3,47 @@ package Tetris.global.config.service;
 import Tetris.global.config.constant.ColorSet;
 import Tetris.global.config.constant.KeyType;
 import Tetris.global.config.constant.WindowSize;
+import Tetris.global.config.dao.ConfigDao;
 import Tetris.global.config.entity.MainConfig;
 
-import java.awt.event.KeyEvent;
+import java.sql.SQLException;
 
 public class ConfigService {
+    private static ConfigService INSTANCE = new ConfigService();
+
+    public static ConfigService getInstance() {
+        return INSTANCE;
+    }
+
+    private MainConfig mainConfig;
+    private ConfigDao configDao;
+
+    public ConfigService() {
+        mainConfig = MainConfig.getInstance();
+        configDao = ConfigDao.getInstance();
+    }
     
-    public void setWindowSize(WindowSize windowSize, MainConfig mainConfig) {
-
+    public void setWindowSize(WindowSize windowSize) throws SQLException {
+        mainConfig.setWindowSize(windowSize);
+        configDao.update();
     }
 
-    public void setColorSet(ColorSet colorSet, MainConfig mainConfig) {
-
+    public void setColorSet(ColorSet colorSet) throws SQLException {
+        mainConfig.setColorSet(colorSet);
+        configDao.update();
     }
 
-    public void setKey(KeyType key, KeyEvent KeyEvent, MainConfig mainConfig) {
-
+    public void setKeyMap(Integer keyEvent, KeyType keyType) throws SQLException {
+        mainConfig.updateKeyMap(keyEvent, keyType);
+        configDao.update();
     }
 
-    public void initConfig(MainConfig mainConfig) {
-
+    public void setDefaultConfig() throws SQLException {
+        mainConfig.setDefault();
+        configDao.update();
     }
 
-    public void initScoreBoard() {
-        
+    public void clearScoreBoard() {
+        // Todo
     }
 }
